19b9f052f09eab4c0ed074ee2da6d5232aa09356
TrangDinh44 <71364100+TrangDinh44@users.noreply.github.com>
2021-06-25 23:17:23 +0700

    Update custom_tokenizer.py

--- custom_tokenizer.py
+++ custom_tokenizer.py
@@ -2,17 +2,14 @@ import spacy
 from spacy.tokenizer import Tokenizer
 import re
 
-def customize_tokenizer(custom_nlp):  
+def customize_tokenizer(nlp):
+  custom_nlp = spacy.load('en_core_web_sm')
   prefix_re = spacy.util.compile_prefix_regex(custom_nlp.Defaults.prefixes)
   suffix_re = spacy.util.compile_suffix_regex(custom_nlp.Defaults.suffixes)
   infix_re = re.compile(r'[]~/:+()\'[",_.>*•#-]')
 
   # Adds support to use special characters listed above as the delimiter for tokenization
-  return Tokenizer(custom_nlp.vocab, prefix_search=prefix_re.search,
+  return Tokenizer(nlp.vocab, prefix_search=prefix_re.search,
                    suffix_search=suffix_re.search,
                    infix_finditer=infix_re.finditer,
                    token_match=None)
-
-# Use 2-line script below to call the customize_tokenizer function:
-#custom_nlp = spacy.load('en_core_web_sm')
-#custom_nlp.tokenizer = customize_tokenizer(custom_nlp)
