112a6f047eb3dd08122a58da9a057b005640777b
TrangDinh44 <71364100+TrangDinh44@users.noreply.github.com>
2021-06-25 20:15:06 +0700

    Update output.py

--- output.py
+++ output.py
@@ -1,15 +1,14 @@
-def modelLoad(nameOnly):
-  from simpletransformers.ner import NERModel
 
-  # Ask for user's GPU option
-  use_cuda = False
-  if str(input("Use GPU?")) == "yes":
-    use_cuda = True
+# Perform NER on abstract content
+def modelNER(samples, modelDir):
+  from simpletransformers.ner import NERModel
 
   # LOAD the newly trained bioBERT model
   model = NERModel('bert', modelDir, use_cuda=use_cuda)
 
   # Analyze the abstract using the model
+  # tokenize abstract content
+  samples = [' '.join([str(x) for x in custom_nlp(i)]) for i in samples]
   # model returns an array in the 'predictions' variable
   predictions, raw_outputs = model.predict(samples)
   
@@ -23,25 +22,33 @@ def modelLoad(nameOnly):
       
   return predictions
 
-# Perform NER on abstract content
-def modelAnalyze(abstCont):
-  model = modelLoad()
-  predAbs = []; predAgs = []
-    
-  #get lists of all predicted tags
-  anaText = ' '.join([str(x) for x in custom_nlp(abstCont)])
-  anaDict = model.analyze(anaText)
-  for ent in anaDict['entities']:
-    if ent['type'] == "Antibody":
-      predAbs.append(ent['text'])
-    else:
-      predAgs.append(ent['text'])
+# Re-format the predicted results
+def predForm(predArr):
+  token = [[] for i in range(len(predArr))]
+  label = [[] for i in range(len(predArr))]
 
-  if nameOnly:
-    predAbs = list(set(predAbs))
-    predAgs = list(set(predAgs))
-      
-  return predAbs, predAgs
+  for i in range(len(predArr)):
+    for dic in predArr[i]:
+      for k, v in dic.items():
+        token[i].append(k)
+        label[i].append(v)
+  
+  startAB = 0; startAG = 0; end = 0
+  trueAb = []; trueAg = []
+  for i in range(len(token)):
+    if label[i] == 'B-Antibody':
+      startAB = i
+    elif label[i] == 'B-Antigen':
+      startAG = i
+    if label[i] != 'I-Antibody' and label[i] != 'I-Antigen':
+      end = i
+      if startAB != 0 and startAB < end:
+        trueAb.append(" ".join(token[startAB:end]))
+        startAB = 0; end = 0
+      elif startAG != 0 and startAG < end:
+        trueAg.append(" ".join(token[startAG:end]))
+        startAG = 0; end = 0
+  return trueAb, trueAg
 
 # Post-processing the output
 def nerOutput(ab_id):
@@ -55,25 +62,36 @@ def nerOutput(ab_id):
   Enter a number (1, 2, or 3): '''))
   
   nerOut = True
-  if str(input('''Do you want to name extraction only or NER visualization? 
+  if str(input('''Do you want name extraction only or NER visualization? 
   Type "y", "yes", or "true" if you want NER visualization: ''')).lower() not in posOpt:
     nerOut = False
 
+  nameOnly = True
+  if str(input('''Do you want the output results to include frequencies of name mentions?
+  Type "y", "yes", or "true" if you want to include frequencies: ''')).lower() in posOpt:
+    nameOnly = False
+    
   saveOut = True
   if str(input('''Do you want to save the output results in a text file?
   Type "y", "yes", or "true" if you want to save: ''')).lower() not in posOpt:
     saveOut = False
+    
+  # Ask for user's GPU option
+  use_cuda = False
+  if str(input('''Are you using GPU?
+  Type "y", "yes", or "true" if GPU is on: ''')).lower() in posOpt:
+    use_cuda = True
+  
+  samples = [v for v in ab_id.values()]
+  modelDir = str(input('Enter the directory where you put the model: '))
+  predAbs, predAgs = predForm(modelNER(samples, modelDir))
   
   text = ""
-
-  for k, v in ab_id.items():
-    predAbs, predAgs = modelAnalyze(v)
-
-    text += "//\nPMID: " + k + "\n"
-    if abagOpt == 1 or abagOpt == 3:
-      text += "Antibody names:\t" + ", ".join(predAbs) + "\n"
-    if abagOpt == 2 or abagOpt == 3:
-      text += "Antigen names:\t" + ", ".join(predAgs) + "\n"
+  text += "//\nPMID: " + k + "\n"
+  if abagOpt == 1 or abagOpt == 3:
+    text += "Antibody names:\t" + ", ".join(predAbs) + "\n"
+  if abagOpt == 2 or abagOpt == 3:
+    text += "Antigen names:\t" + ", ".join(predAgs) + "\n"
 
   print(text)
 
