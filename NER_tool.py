49f5f140e6e9e8ff5c967f981dfa4854271a8227
TrangDinh44 <71364100+TrangDinh44@users.noreply.github.com>
2021-06-25 23:21:00 +0700

    Update NER_tool.py

--- NER_tool.py
+++ NER_tool.py
@@ -2,11 +2,6 @@ from custom_tokenizer import customize_tokenizer
 from input import nerInput
 from output import nerOutput
 
-import spacy
-# Create a customized tokenizer
-custom_nlp = spacy.load('en_core_web_sm')
-custom_nlp.tokenizer = customize_tokenizer(custom_nlp)
-
 # Input
 ab_id = nerInput()
 
