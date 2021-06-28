7239626abed0a2cdfaf7182534660cab445a71ce
TrangDinh44 <71364100+TrangDinh44@users.noreply.github.com>
2021-06-28 18:58:00 +0700

    Update output.py

--- output.py
+++ output.py
@@ -88,7 +88,8 @@ def nerOutput(ab_id):
     Type 3 for both AB and AG
     Enter a number (1, 2, or 3): '''))
   except ValueError:
-    return "Your option is INVALID! Please try again and enter 1, 2, or 3 as an INTEGER."
+    "Your option is INVALID! Please try again and enter 1, 2, or 3 as an INTEGER."
+    return
 
   allMent = userOpt('''The same ABAG names may appear multiple times in an abstract.
   Do you want the output results to list all such mentions or just unique names?
